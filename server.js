f1195da857faa2ae013cce935a36e83f8e8803eb
Stacey Teltser <stacey.teltser@gmail.com>
2021-06-29 13:31:00 -0500

    rendering weather json data to /weather endpoint but just sending it as raw data for now

--- server.js
+++ server.js
@@ -8,4 +8,11 @@ app.get('/', (request, response) => {
   response.send('helllllloooo!');
 });
 
+const weatherData = require('./data/weather.json');
+
+app.get('/weather', (request, response) => {
+  response.send(weatherData);
+});
+
+
 app.listen(PORT, () => console.log(`app is listening on port ${PORT}`));
