e4f6b3366023ebc5f293ad37c283dd872f804ba3
Stacey Teltser <stacey.teltser@gmail.com>
2021-06-29 13:20:59 -0500

    making basic get request to home route

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.get('/', (request, response) => {
+  response.send('helllllloooo!');
+});
+
 app.listen(PORT, () => console.log(`app is listening on port ${PORT}`));
