568ef2e33f9ed4df7665711a33525ba54d22ae0c
Stacey Teltser <stacey.teltser@gmail.com>
2021-07-13 13:44:52 -0500

    modularized movie handler logic

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 
 const express = require('express');
 const cors = require('cors');
-const superagent = require('superagent');
+const getMovies = require('./movies');
 const getWeather = require('./weather');
+
 
 require('dotenv').config();
 
@@ -20,29 +21,6 @@ app.get('/weather', getWeather);
 
 app.get('/movies', getMovies);
 
-function getMovies(request, response) {
-  const url = 'https://api.themoviedb.org/3/search/movie';
-  const query = {
-    api_key: process.env.MOVIE_API_KEY,
-    query: request.query.search_query,
-  }
-  superagent.get(url)
-    .query(query)
-    .then(movieResponse => {
-      const movies = movieResponse.body.results.map(movie => new Movie(movie));
-      response.status(200).send(movies);
-    })
-}
-
-function Movie(movie) {
-  this.title = movie.original_title;
-  this.overview = movie.overview;
-  this.average_votes = movie.vote_average;
-  this.total_votes = movie.vote_count;
-  this.image_url = movie.poster_path;
-  this.popularity = movie.popularity;
-  this.released_on = movie.release_date;
-}
 
 // function handleError(err, response) {
 //   response.status(500).send('Whoops. Internal error.');
