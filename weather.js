7dbda7ea027ba48088ba67aeb367eb0560171f1d
Stacey Teltser <stacey.teltser@gmail.com>
2021-07-15 13:52:19 -0500

    removed seperate cache file, if new search store data in cache otherwise send cached data

--- weather.js
+++ weather.js
@@ -1,19 +1,28 @@
 const superagent = require('superagent');
-const cache = require('./cache');
+const cache = {};
 
 function getWeather(request, response) {
-  const url = 'https://api.openweathermap.org/data/2.5/onecall';
-  const query = {
-    appid: process.env.WEATHER_API_KEY,
-    lat: request.query.lat,
-    lon: request.query.lon,
+  const key = `weather-${request.query.lat}${request.query.lon}`;
+  if(!cache[key]){
+    const url = 'https://api.openweathermap.org/data/2.5/onecall';
+    const query = {
+      appid: process.env.WEATHER_API_KEY,
+      lat: request.query.lat,
+      lon: request.query.lon,
+      }
+      superagent.get(url)
+        .query(query)
+        .then(weatherResponse => {
+          const dailyForecasts = weatherResponse.body.daily.map(day => new Forecast(day));
+          cache[key] = dailyForecasts;
+          console.log('cache miss');
+          response.status(200).send(dailyForecasts);
+        })
+  } else {
+      let cachedData = cache[key];
+      console.log('Cache hit');
+      response.status(200).send(cachedData);
     }
-    superagent.get(url)
-      .query(query)
-      .then(weatherResponse => {
-        const dailyForecasts = weatherResponse.body.daily.map(day => new Forecast(day));
-        response.status(200).send(dailyForecasts);
-    })
 }
 
 function Forecast(day) {
